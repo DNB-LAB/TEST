9ad1bd512f81a7d3ded88892c2cb3e973feb949a
Chunghee Lee <bluehe2@naver.com>
2025-03-02 22:18:39 +0900

    12th comment

--- tree/menu00.js
+++ tree/menu00.js
@@ -1,14 +1,12 @@
 //소스수정자 : 이청희
-  foldersTree = gFld("<span style=font-size:9pt;><b><font color=#000000>&nbsp;정보공유 시스템", "index.html")
-	 insDoc(foldersTree, gLnk(2, "<span style=font-size:9pt;><font color=#000000>시장출하 적부 판정", "021_Judge_System.html"))
-	insDoc(foldersTree, gLnk(2, "<span style=font-size:9pt;><font color=#000000>적합 로트 리스트(20/01~22/03)", "023_Judge_Lot_List.html"))
-	 
+  foldersTree = gFld("<span style=font-size:9pt;><b><font color=#000000>&nbsp;코스메틱 오케스트라라", "index.html")
+		 
 	aux1 = insFld(foldersTree, gFld("<span style=font-size:9pt;><font color=#000000>제품 정보 자료실", ""))	
 	 insDoc(foldersTree, gLnk(2, "<span style=font-size:9pt;><font color=#000000>벌크 제품 Db(화장품)", "031_Bulk_Goods.html"))
 	 insDoc(foldersTree, gLnk(2, "<span style=font-size:9pt;><font color=#000000>완제품 Db(화장품)", "032_Finsh_Goods.html"))
 	 insDoc(foldersTree, gLnk(2, "<span style=font-size:9pt;><font color=#000000>벌크 제품 Db(기타)", "033_Bulk_Goods_Other.html"))
 	 insDoc(foldersTree, gLnk(2, "<span style=font-size:9pt;><font color=#000000>완제품 Db(기타)", "034_Finsh_Goods_Other.html"))
-   insDoc(foldersTree, gLnk(2, "<span style=font-size:9pt;><font color=#000000>자사 마스크 철형 Db", "035_Mask_Frame.html"))
+   
    
    
  aux1 = insFld(foldersTree, gFld("<span style=font-size:9pt;><font color=#000000>제품 안정도 자료실", ""))	
@@ -22,12 +20,8 @@
   
    
  aux1 = insFld(foldersTree, gFld("<span style=font-size:9pt;><font color=#000000>이력 관련 자료실", ""))	  
-  insDoc(foldersTree, gLnk(2, "<span style=font-size:9pt;><font color=#000000>신제품 라인 트라이얼 자료실", "501_Paking_Report.html")) 
-	insDoc(foldersTree, gLnk(2, "<span style=font-size:9pt;><font color=#000000>자재 테스트 자료실", "503_Sub_Material_Test.html")) 
-	
-aux1 = insFld(foldersTree, gFld("<span style=font-size:9pt;><font color=#000000>더 엔엠에프 앰플 마스크", ""))	  
-  insDoc(foldersTree, gLnk(2, "<span style=font-size:9pt;><font color=#000000>더 엔엠에프 앰플 마스크 정품 확인", "701_NMF_Original_Test.html")) 
-  insDoc(foldersTree, gLnk(2, "<span style=font-size:9pt;><font color=#000000>원료 분석 Data", "702_HYDRODEEPER_Data.html")) 
+ 	insDoc(foldersTree, gLnk(2, "<span style=font-size:9pt;><font color=#000000>자재 테스트 자료실", "503_Sub_Material_Test.html")) 
+	insDoc(foldersTree, gLnk(2, "<span style=font-size:9pt;><font color=#000000>원료 분석 Data", "702_HYDRODEEPER_Data.html")) 
   
   
  aux1 = insFld(foldersTree, gFld("<span style=font-size:9pt;><font color=#000000>각종 자료실", ""))	  
